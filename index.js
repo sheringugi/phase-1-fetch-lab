ff3d3dce35ab6902faf5a8ba6acb7beaf48b2b01
sheringugi <sheri.ngugi17@gmail.com>
2023-03-27 11:55:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
 function fetchBooks() {
-  fetch("https://anapioficeandfire.com/api/books")
+  return fetch('https://anapioficeandfire.com/api/books')
   .then((resp) => resp.json())
-  .then((json) => renderBooks(json));
-  
-  
+  .then((data) => renderBooks(data));
 }
+
 function renderBooks(books) {
   const main = document.querySelector('main'); 
-  let array= books
-  array.forEach(book => {
-    array.push(book)
+  books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = `<h2>${book.name}<h2>`;
     main.appendChild(h2);
-
   });
-  return books
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
-
-
-
-
